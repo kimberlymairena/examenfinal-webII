afdc8b426f1003664dd0fb2824f9e9814c2e176d
Kimberly Mairena <126621195+kimberlymairena@users.noreply.github.com>
2023-06-19 22:48:11 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,97 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'AppBase',
+  	routes: [
+		{
+		path: '/index/',
+    	url: 'index.html',
+    	name: 'index',
+  		},
+		{
+		path: '/contenido/',
+    	url: 'contenido.html',
+    	name: 'contenido',
+  		},
+		{
+		path: '/listado2/',
+    	url: 'listado2.html',
+    	name: 'listado2',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+		{
+		path: '/detalle/',
+    	url: 'detalle.html',
+    	name: 'detalle',
+  		},
+	],
+	dialog: {
+		title: 'AppBase',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#login');
+		
+		app.dialog.alert('!Welcome to AppBase !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnAddCart', function (e) {
+	e.preventDefault();
+
+	
+	var notification = app.notification.create({
+      icon: '<i class="material-icons">check</i>',
+      title: 'Order',
+      titleRightText: '',
+      subtitle: '',
+      text: "Your order has been received.",
+      closeTimeout: 3000,
+    });
+    notification.open();
+	
+});
+
+
+
+
+
